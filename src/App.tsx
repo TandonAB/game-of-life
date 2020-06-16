81c9782df234a75eccb1d1406f00282b3d2ea790
Abhishek Tandon <web.tandon@gmail.com>
2020-06-16 16:07:12 +0530

    Game of life with hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,129 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useRef } from 'react';
+import produce from 'immer';
 
-function App() {
+const numRows = 30;
+const numCols = 30;
+
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0]
+];
+
+const generateEmptyGid = () => {
+  const rows = [];
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => 0));
+  }
+
+  return rows;
+}
+
+const App: React.FC = () => {
+  const [grid, setGrid] = useState(() => {
+    return generateEmptyGid();
+  });
+
+  const [running, setRunning] = useState(false);
+
+  const runningRef = useRef(running);
+  runningRef.current = running;
+
+  const runSimulation = useCallback(
+    () => {
+      if (!runningRef.current) {
+        return;
+      }
+
+      setGrid(g => {
+        return produce(g, gridCopy => {
+          for (let i = 0; i < numRows; i++) {
+            for (let k = 0; k < numCols; k++) {
+              let neighbors = 0;
+              operations.forEach(([x, y]) => {
+                const newI = i + x;
+                const newK = k + y;
+
+                if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                  neighbors += g[newI][newK];
+                }
+              })
+
+              if (neighbors < 2 || neighbors > 3) {
+                gridCopy[i][k] = 0;
+              } else if (g[i][k] === 0 && neighbors === 3) {
+                gridCopy[i][k] = 1;
+              }
+            }
+          }
+        })
+      })
+
+      setTimeout(runSimulation, 300);
+    },
+    [],
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => {
+        setRunning(!running)
+        if (!running) {
+          runningRef.current = true;
+          runSimulation();
+        }
+      }}>
+        {running ? 'Stop' : 'Start'}
+      </button>
+
+      <button
+        onClick={() => {
+          setGrid(generateEmptyGid());
+        }}
+      >
+        Clear
+      </button>
+
+      <button
+        onClick={() => {
+          const rows = [];
+          for (let i = 0; i < numRows; i++) {
+            rows.push(Array.from(Array(numCols), () => Math.random() > 0.5 ? 1 : 0));
+          }
+          setGrid(rows);
+        }}
+      >
+        Random
+      </button>
+
+      <div style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat(${numCols}, 20px)`
+      }}>
+        {grid.map((rows, i) => rows.map((col, k) => (
+          <div
+            key={`${i}-${k}`}
+            onClick={() => {
+              const newGrid = produce(grid, gridCopy => {
+                gridCopy[i][k] = grid[i][k] ? 0 : 1;
+              })
+              setGrid(newGrid);
+            }}
+            style={{
+              width: 20,
+              height: 20,
+              border: '1px solid #a5a5a5',
+              backgroundColor: grid[i][k] ? "pink" : undefined
+            }}
+          />
+        )))}
+      </div>
+    </>
   );
 }
 
